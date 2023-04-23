0c074e27decaa899dc25e469b6226e7443f514e0
Vladimir Varenik <117539159+VVV33301@users.noreply.github.com>
2023-04-23 21:10:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -146,7 +146,6 @@ ytdl_format_options = {'format': 'bestaudio/best', 'outtmpl': '%(extractor)s-%(i
                        'ignoreerrors': False, 'logtostderr': False, 'quiet': True, 'no_warnings': True,
                        'default_search': 'auto', 'source_address': '0.0.0.0'}
 ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 spam_flag = False  # Разрешение на спам
 ban_words, ban_roots = [], []  # Списки с запрещенными корнями и словами
